199e6b82d8fe6d6942e72e5b3af94b0a4e968f9f
Tarusov Dmitriy <tarusovtdv@gmail.com>
2023-07-24 15:45:27 +0300

    changed the mining page component

--- src/components/PictureMain.vue
+++ src/components/PictureMain.vue
@@ -4,28 +4,28 @@
             <div class="main__toolbar">
                 <div class="main__autor-info">
                     <div class="main__autor-photo">
-                        <img src="" alt="">
+                        <img :src="getPhotoFromStore.user.profile_image.small" alt="imgAuthorError">
                     </div>
                     <div class="main__autor-contacts">
-                        <div class="main__autor-name">lol</div>
-                        <div class="main__autor-links">kek</div>
+                        <div v-if="getPhotoFromStore.user.name" class="main__autor-name">{{getPhotoFromStore.user.name }}</div>
+                        <div v-if="getPhotoFromStore.user.twitter_username" class="main__autor-links">@ {{ getPhotoFromStore.user.twitter_username}}</div>
                     </div>
                 </div>
                 <div class="main__buttons">
-                    <button class="main__like"></button>
+                    <button class="main__like-btn">
+                        <img src="@/assets/BlackHeart.svg" alt="photoError">
+                    </button>
                     <button class="main__download">
-                        <img src="" alt="photoError">
+                        <img src="@/assets/DownLoad.svg" alt="photoError">
                         <p>Download</p>
                     </button>                    
                 </div>
             </div>
         </div>
-        <div v-if="getPhotoFromStore">
-            <img :src="getPhotoFromStore.urls.regular" alt="errorImagePath">
+        <div class="main__picture-container">
+            <img :src="getPhotoFromStore.urls.regular" alt="errorImagePath" class="main__picture">
         </div>
-        <div v-else>
-            huyta kakayato
-        </div>
+        <img :src="getPhotoFromStore.urls.regular" alt="errorImagePath" class="main__picture-bg">
     </div>
 </template>
 
@@ -52,7 +52,115 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.main__container{
+@import '@/mixins/mixinLayout.scss';
+
+.main__container {
     background-color: #00000050;
+    position: relative;
+    overflow: hidden;
+    padding: 2.2vw $offset;
 }
+
+.main__toolbar {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 2vw;
+}
+.main__autor-info {
+    display: flex;
+    gap: 10px;
+}
+
+.main__autor-photo {
+    border:#fff 1px solid;
+    width: 2.8vw;
+    height: 2.8vw;
+    border-radius: $borderRad;
+    overflow: hidden;
+}
+
+.main__autor-photo img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.main__autor-contacts { 
+    color: #fff;
+}
+
+.main__autor-name {
+    font-size: 28px;
+}
+
+.main__autor-links {
+    font-size: 18px;
+}
+.main__buttons {
+    display: flex;
+    gap: 1vw;
+}
+
+.main__like-btn {
+    background: #ffffff;
+    border: none;
+    border-radius: $borderRad;
+    width: 2.54vw;
+    height: 2.54vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.main__like-btn:active {
+    background: #00000050;
+    transition: ease-in-out .08s;
+    
+}
+.main__like-btn img {
+    width: 1.12vw;
+}
+.main__download {
+    width: 10.4vw;
+    height: 2.54vw;
+    display: flex;
+    align-items: center;
+    background-color: #FFF200;
+    border-radius: $borderRad;
+    border: none;
+    gap: 0.8vw;
+    padding: 0.6vw;
+    transition: ease-in-out .08s;
+}
+
+.main__download:active {
+    background: #FFF20050;
+}
+
+.main__download img {
+    width: 1.4vw;
+}
+
+.main__download p {
+    font-size: 20px;
+}
+
+.main__picture-container {
+    overflow: hidden;
+    height: 38vw;
+}
+
+.main__picture {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+}
+.main__picture-bg {
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    z-index: -1; 
+}
+
 </style>
